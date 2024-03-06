dd6bdca27b8ed66a6882948012d6ec6b14fb436c
Danylo-Ik <daniel.ik0806@gmail.com>
2024-03-06 14:05:23 +0200

    lol

--- user/routes.py
+++ user/routes.py
@@ -2,6 +2,6 @@ from flask import Flask
 from app import app
 from user.models import User
 
-@app.route('/user/registration_form', methods=['POST'])
+@app.route('/user/signup', methods=['POST'])
 def signup():
     return User().signup()
