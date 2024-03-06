1306eeacd159b26d705bd2a5642e1a247ae50d27
Danylo-Ik <daniel.ik0806@gmail.com>
2024-03-06 18:25:49 +0200

    Add user signout functionality and login route*** ***Update registration form template*** ***Add exercises_extention template*** ***Update User model to handle user sessions*** ***Update app.py to include new routes and templates

--- app.py
+++ app.py
@@ -1,27 +1,63 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session, url_for
+from functools import wraps
 from pymongo import MongoClient
+import certifi
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
-client = MongoClient("mongodb+srv://Danyil:m1MaJ0ADwtgm0mso@gymassistant.zuau6ap.mongodb.net/")
+app.secret_key = 'somesthing'
+connection = "mongodb+srv://Danyil:m1MaJ0ADwtgm0mso@gymassistant.zuau6ap.mongodb.net/"
+client = MongoClient(connection, tlsCAFile=certifi.where())
+
 db = client['gymassistant']
 
+all_exercises = db['all_exercises']
+
 from user import routes
+
+# def login_required(f):
+#     @wraps(f)
+#     def wrap(*args, **kwargs):
+#         if 'logged_in' in session:
+#             return f(*args, **kwargs)
+#         else:
+#             return redirect('/')
+#     return wrap
 
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
+# @app.route('/my_profile/')
+# @login_required
+# def my_profile():
+#     return render_template('my_profile.html')
+
+
 @app.route('/my_profile/')
 def my_profile():
     return render_template('my_profile.html')
 
-@app.route('/exercises/')
-def exercises():
-    return render_template('exercises.html')
+@app.route('/my_training')
+def my_training():
+    return redirect(url_for('my_profile')+'#training')
 
 @app.route('/registration_form/')
 def registration_form():
     return render_template('registration_form.html')
+
+@app.route('/login_form/')
+def login_form():
+    return render_template('login.html')
+
+@app.route('/triceps')
+def triceps():
+    trainings = all_exercises.find()
+    return render_template('exercises_extention.html', trainings=trainings, muscle = "М'язи тріцепса")
+
+@app.route('/biceps')
+def biceps():
+    trainings = all_exercises.find()
+    return render_template('exercises_extention.html', trainings=trainings, muscle = "М'язи біцепса")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- user/models.py
+++ user/models.py
@@ -1,9 +1,14 @@
-from flask import Flask, jsonify, request, render_template
+from flask import Flask, jsonify, request, render_template, session, redirect
 import uuid
 from passlib.hash import pbkdf2_sha256
 from app import db
 
 class User:
+    def start_session(self, user):
+        session['logged_in'] = True
+        session['user'] = user
+        return redirect('/my_profile')
+
     def signup(self):
         print(request.form)
 
@@ -21,6 +26,20 @@ class User:
             return jsonify({"error" : "Email address already in use"}), 400
 
         if db['users'].insert_one(user):
-            return render_template('main_page.html')
+            return self.start_session(user)
 
         return jsonify({"error" : "Signup failed"}), 400
+    
+    def signout(self):
+        session.clear()
+        return redirect('/')
+    
+    def login(self):
+        user = db['users'].find_one({
+            "email" : request.form.get('email')
+        })
+
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+
+        return jsonify({"error" : "Invalid login credentials"}), 401
