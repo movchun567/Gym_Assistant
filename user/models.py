dd1a1152495ac111367656120e59c1b0f5c46370
MariaOnyshchuk <onyshchuk.pn@ucu.edu.ua>
2024-03-12 21:13:54 +0200

    ПОФІКСИЛА БАГ ТУДА ААААААААААААААААА

--- user/models.py
+++ user/models.py
@@ -54,7 +54,7 @@ class User:
         return render_template('login.html', error_message=error_message)
     
     def save_training(self):
-        user_id = session['user']['_id'] # query to find the user's document)        
+        user_id = session['user']['_id'] # query to find the user's document)
         if request.form.get('training_name') in users.find_one({'_id': user_id})['trainings_name']:
             users.update_one({'_id': user_id},  # query to find the user's document
             {'$pull': {'trainings_name': request.form.get('training_name')}})  # update operation
@@ -76,13 +76,13 @@ class User:
     #     {'$push': {'trainings': { 'training_url': request.form.get('training_url'), 'training_name': request.form.get('training_name'), 'training_description': request.form.get('training_description')}}})
     #     return '', 204
     
-    def del_training(self):
-        user_id = session['user']['_id']
-        users.update_one({'_id': user_id},  # query to find the user's document
-        {'$pull': {'trainings_name': request.form.get('training_name')}})  # update operation
-        users.update_one({'_id': user_id},
-        {'$pull': {'trainings': { 'training_url': request.form.get('training_url'), 'training_name': request.form.get('training_name'), 'training_description': request.form.get('training_description')}}})
-        return '', 204
+    # def del_training(self):
+    #     user_id = session['user']['_id']
+    #     users.update_one({'_id': user_id},  # query to find the user's document
+    #     {'$pull': {'trainings_name': request.form.get('training_name')}})  # update operation
+    #     users.update_one({'_id': user_id},
+    #     {'$pull': {'trainings': { 'training_url': request.form.get('training_url'), 'training_name': request.form.get('training_name'), 'training_description': request.form.get('training_description')}}})
+    #     return '', 204
     
     # def profile_image(self):
     #     # fs = gridfs.GridFS(db)
